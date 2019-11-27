f0e60e3607b40836018b2b4dfb50e15bb7ac9ff8
Robert M Ricci <rmricci121@gmail.com>
2019-11-27 00:51:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,7 @@ end
 def big_shoe_rebounds
    game_hash.map do |place, team|
     team[:players].each do |player|
-      size = player[:shoe].max_by
+      size = player[:shoe]
         # binding.pry
     end
   end
